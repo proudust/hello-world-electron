92dd342c35a6c3da661e0e652ca4247c7647e8c2
Proudust <proudust@gmail.com>
2020-01-29 22:47:49 +0900

    :heavy_plus_sign: electron-builder を追加

---
+++ electron-builder.js
@@ -0,0 +1,12 @@
+module.exports = {
+  appId: 'io.github.proudust.helloworld-electron',
+  win: {
+    target: 'portable'
+  },
+  mac: {
+    target: 'zip'
+  },
+  linux: {
+    target: 'zip'
+  }
+}
